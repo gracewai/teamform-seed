a182cf63c1194f0be7f37c3a5a29cb7de2455f86
XDrzzz <dannyng10231996@gmail.com>
2016-11-29 22:27:16 +0800

    UI
    UI

--- app/js/admin.js
+++ app/js/admin.js
@@ -1,93 +1,158 @@
-var admin_ready = function() {
+
+var admin_ready = function(){
+
 	$('#admin_page_controller').hide();
-	var eventName = getURLParameter("q");
+	var eventName = getURLParameter("event");
 	$('#text_event_name').text("Event name: " + eventName);
-	if(eventName == null || eventName.trim() == "") {
+	if(eventName == null || eventName.trim() == ""){
 		$('#text_event_name').text("Error: Invalid event name ");
 	}
+
 }
+
 
 $(document).ready(admin_ready);
 
-angular.module('teamform-admin-app', ['firebase'])
-.controller('AdminCtrl', ['$scope', '$firebaseObject', '$firebaseArray', function($scope, $firebaseObject, $firebaseArray) {
 
-	// TODO: implementation of AdminCtrl
+angular.module("teamform-admin-app", ["firebase", "ngMaterial", "ngMessages"])
+.controller("AdminCtrl", function($scope, $firebaseObject, $firebaseArray, $window, $mdDialog) {
 
-	// Initialize $scope.param as an empty JSON object
-	$scope.param = {};
+    // TODO: implementation of AdminCtrl
+    // Initialize $scope.param as an empty JSON object
+    $scope.param = {};
+    // Call Firebase initialization code defined in site.js
+    initializeFirebase();
+    var refPath, ref, eventName;
+    eventName = getURLParameter("event");
+    if (eventName === null) {
+        eventName = "test";
+    }
+    refPath = eventName + "/admin/param";
+    ref = firebase.database().ref("events/" + refPath);
+    // Link and sync a firebase object
+    $scope.param = $firebaseObject(ref);
+    $scope.paramMess="";
+    $scope.paramLdMess=function(){
+//  $scope.param.$loaded()
+//      .then(function(data) {
 
-	// Call Firebase initialization code defined in site.js
-	initalizeFirebase();
+            // Fill in some initial values when the DB entry doesn't exist
+            if(typeof $scope.param.maxTeamSize == "undefined"){
+                $scope.param.maxTeamSize = 10;
+                $scope.paramMess+="NoMax "
+            }
+            if(typeof $scope.param.minTeamSize == "undefined"){
+                $scope.param.minTeamSize = 1;
+                $scope.paramMess+="NoMin "
+            }
 
-	var refPath, ref, eventName;
+            // Enable the UI when the data is successfully loaded and synchornized
+            $('#admin_page_controller').show();
+        };
+        $scope.param.$loaded().then($scope.paramLdMess);
+    refPath = eventName + "/team";
+    $scope.team = [];
+    $scope.team = $firebaseArray(firebase.database().ref("events/" + refPath));
+    refPath = eventName + "/member";
+    $scope.member = [];
+    $scope.member = $firebaseArray(firebase.database().ref("events/" + refPath));
+    $scope.changeMinTeamSize = function(delta) {
+        var newVal = $scope.param.minTeamSize + delta;
+        if (newVal >= 1 && newVal <= $scope.param.maxTeamSize) {
+            $scope.param.minTeamSize = newVal;
+        }
+        $scope.param.$save();
+    };
+    $scope.changeMaxTeamSize = function(delta) {
+        var newVal = $scope.param.maxTeamSize + delta;
+        if (newVal >= 1 && newVal >= $scope.param.minTeamSize) {
+            $scope.param.maxTeamSize = newVal;
+        }
+        $scope.param.$save();
+    };
+    $scope.saveFunc = function() {
+        $scope.param.$save();
+        // Finally, go back to the front-end
+        $window.open("index.html", "_self");
+    };
 
-	eventName = getURLParameter("q");
-	refPath = eventName + "/admin/param";
-	ref = firebase.database().ref(refPath);
+    // Date
+    $scope.startDate = new Date();
+    $scope.endDate = new Date();
 
-	// Link and sync a firebase object
-	$scope.param = $firebaseObject(ref);
-	$scope.paramMess="";
-	$scope.paramLdMess=function(){
-//	$scope.param.$loaded()
-//		.then(function(data) {
+    var eventAdminParamRef = firebase.database().ref().child("events").child(eventName).child("admin").child("param");
+    var eventAdminParamObj = $firebaseObject(eventAdminParamRef);
+    eventAdminParamObj.$loaded().then(function(admin) {
+        $scope.startDate = new Date(admin.startDate);
+        $scope.endDate = new Date(admin.endDate);
+        $scope.details = admin.details;
+        console.log($scope.startDate);
+        console.log($scope.endDate);
+        console.log($scope.details);
 
-			// Fill in some initial values when the DB entry doesn't exist
-			if(typeof $scope.param.maxTeamSize == "undefined"){
-				$scope.param.maxTeamSize = 10;
-				$scope.paramMess+="NoMax "
-			}
-			if(typeof $scope.param.minTeamSize == "undefined"){
-				$scope.param.minTeamSize = 1;
-				$scope.paramMess+="NoMin "
-			}
+        if (admin.startDate == null && admin.endDate == null) {
+            $scope.startDate = new Date();
+            $scope.endDate = new Date();
+        }
+        if (admin.details == null) {
+            $scope.details = null;
+        }
+    });
 
-			// Enable the UI when the data is successfully loaded and synchornized
-			$('#admin_page_controller').show();
-		};
-		$scope.param.$loaded().then($scope.paramLdMess);
-//		})
-//		.catch(function(error) {
-			// Database connection error handling...
-			//console.error("Error:", error);
-//		});
+    $scope.minDate = new Date();
+    $scope.startChange = function() {
+        $scope.minDate = $scope.startDate;
+    };
 
-	refPath = "users";
-	$scope.users = [];
-	$scope.users = $firebaseArray(firebase.database().ref(refPath));
-	console.log($scope.users);
+    $scope.saveContent = function() {
+        if ($scope.details == null || $scope.startDate == null || $scope.endDate == null) {
+            return;
+        }
 
-	refPath = eventName + "/team";
-	$scope.team = [];
-	$scope.team = $firebaseArray(firebase.database().ref(refPath));
+        //console.log($scope.startDate);
+        //console.log($scope.details);
+        ref.update({'startDate': $scope.startDate.getTime(), 'endDate': $scope.endDate.getTime(),
+            'details': $scope.details});
+    };
+
+    $scope.zeroMember = function(teamMembers) {
+        if (teamMembers === undefined) {
+            return true;
+        } else {
+            return false;
+        }
+    };
 
 
-	refPath = eventName + "/member";
-	$scope.member = [];
-	$scope.member = $firebaseArray(firebase.database().ref(refPath));
+    // confirm automatic team form
+    $scope.confirmAutomaticTeamForm = function(callback) {
+        var confirm = $mdDialog.confirm()
+            .title("Automatic Team Form")
+            .ok("Form")
+            .cancel("Cancel");
 
-	$scope.changeMinTeamSize = function(delta) {
-		var newVal = $scope.param.minTeamSize + delta;
-		if (newVal >=1 && newVal <= $scope.param.maxTeamSize ) {
-			$scope.param.minTeamSize = newVal;
-		}
-		$scope.param.$save();
-	}
+        $mdDialog.show(confirm)
+            .then(function() {
+                callback(true);
+            }, function() {
+                callback(false);
+            });
+    };
 
-	$scope.changeMaxTeamSize = function(delta) {
-		var newVal = $scope.param.maxTeamSize + delta;
-		if (newVal >=1 && newVal >= $scope.param.minTeamSize ) {
-			$scope.param.maxTeamSize = newVal;
-		}
+    // automatic team form
+    $scope.automaticTeamForm = function() {
+        $scope.confirmAutomaticTeamForm(function(confirm) {
+            if (!confirm) {
+                // document.querySelector(".mdl-js-snackbar").MaterialSnackbar.showSnackbar({message: "Cancel automatic team form"});
+                return;
+            }
 
-		$scope.param.$save();
-	}
-
-	$scope.saveFunc = function() {
-		$scope.param.$save();
-		// Finally, go back to the front-end
-		window.location.href= "index.html";
-	}
-
-}]);
+            console.log("automatic team form");
+        });
+    };
+})
+.config(function($mdThemingProvider) {
+    $mdThemingProvider.theme('default')
+    .primaryPalette('lime')
+    .accentPalette('amber');
+});
